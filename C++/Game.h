825b2a7d58be16794676c4edd48e955c3581edb4
csuvbakka <csuvbakka@gmail.com>
2021-11-28 15:55:09 +0100

    rearranged public and private members declaration order

--- C++/Game.h
+++ C++/Game.h
@@ -6,12 +6,23 @@
 #define GAME_H_
 
 class Game {
- private:
-  std::vector<std::string> players;
+ public:
+  Game();
+  bool isPlayable();
+  bool add(std::string playerName);
 
+  void roll(int roll);
+  bool wasCorrectlyAnswered();
+  bool wrongAnswer();
+
+ private:
+  bool didPlayerWin();
+  void askQuestion();
+  std::string currentCategory();
+
+  std::vector<std::string> players;
   int places[6];
   int purses[6];
-
   bool inPenaltyBox[6];
 
   std::list<std::string> popQuestions;
@@ -21,24 +32,6 @@ class Game {
 
   unsigned int currentPlayer;
   bool isGettingOutOfPenaltyBox;
-
- public:
-  Game();
-  bool isPlayable();
-  bool add(std::string playerName);
-
-  void roll(int roll);
-
- private:
-  void askQuestion();
-  std::string currentCategory();
-
- public:
-  bool wasCorrectlyAnswered();
-  bool wrongAnswer();
-
- private:
-  bool didPlayerWin();
 };
 
 #endif /* GAME_H_ */
